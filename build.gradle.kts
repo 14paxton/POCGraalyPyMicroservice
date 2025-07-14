ae57d2b5e4176dca1e9a4d07be30c5664615d995
BP <26972590+14paxton@users.noreply.github.com>
2025-07-14 08:28:48 -0700

    copied

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,226 +1,202 @@
-import PipInstall.PackageName
 import PipInstall.PackageName.*
-import PipInstall.getPackageBinary
 import PipInstall.wheelOsStandard
 
-val numpyInstall = getPackageBinary(rootDir, NUMPY)
-val paddlePaddleInstall = getPackageBinary(rootDir, PADDLEPADDLE)
-val paddleOcrInstall = getPackageBinary(rootDir, PADDLEOCR)
-val paddleXInstall = getPackageBinary(rootDir, PADDLEX)
-val sciPyInstall = getPackageBinary(rootDir, SCIPY)
-val pandasInstall = getPackageBinary(rootDir, PANDAS)
-val skitLearnInstall = getPackageBinary(rootDir, SKITLEARN)
-val pillowInstall = getPackageBinary(rootDir, PILLOW)
-val shapelyInstall = getPackageBinary(rootDir, SHAPELY)
-val tikTokenInstall = getPackageBinary(rootDir, TIKTOKEN)
+val graalPythonVersion: String by project
+
+val paddlePaddleInstall = PADDLEPADDLE.getPackage(rootDir)
+val paddleOcrInstall = PADDLEOCR.getPackage(rootDir)
+val sciPyInstall = SCIPY.getPackage(rootDir)
+val pandasInstall = PANDAS.getPackage(rootDir)
+val sciKitLearnInstall = SCIKIT_LEARN.getPackage(rootDir)
+val shapelyInstall = SHAPELY.getPackage(rootDir)
+val tikTokenInstall = TIKTOKEN.getPackage(rootDir)
 
 plugins {
-    id("io.micronaut.application") version "4.5.4"
-    id("org.graalvm.python") version "24.2.1"
-    id("com.gradleup.shadow") version "8.3.6"
-    id("io.micronaut.aot") version "4.5.3"
+  id("io.micronaut.application") version "4.5.4"
+  id("org.graalvm.python") version "24.2.1"
+  id("com.gradleup.shadow") version "8.3.8"
+  id("io.micronaut.aot") version "4.5.3"
 }
-
-// **************************************************************************************************************************
-// PYTHON LIBRARIES Import ***********************************************************************************************
-
-graalPy {
-    packages.set(
-            setOf(
-                    "--prefer-binary",
-                    wheelOsStandard,
-                    numpyInstall,
-                    "python-dotenv==1.1.1",
-                    "tqdm==4.67.1",
-                    "PyYAML==6.0.2",
-                    "pydantic==2.11.7",
-                    tikTokenInstall,
-                    pillowInstall,
-                    shapelyInstall,
-                    skitLearnInstall,
-                    pandasInstall,
-                    sciPyInstall,
-                    paddlePaddleInstall,
-                    paddleOcrInstall,
-                    paddleXInstall,
-                 )
-                )
-}
-
-// END PYTHON LIBRARIES Import *********************************************************************************************
-// *************************************************************************************************************************
 
 group = "com.nameplate"
 version = "0.1"
 
 repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
-dependencies {
-    compileOnly("io.micronaut:micronaut-http-client")
-
-    implementation("org.graalvm.polyglot:polyglot:24.2.1")
-    implementation("org.graalvm.polyglot:python:24.2.1")
-    implementation("io.micronaut.views:micronaut-views-thymeleaf")
-    implementation("io.micronaut:micronaut-http-server-netty")
-    implementation("io.micronaut.graal-languages:micronaut-graalpy")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
-
-    runtimeOnly("org.yaml:snakeyaml")
-    runtimeOnly("ch.qos.logback:logback-classic")
-
-    testImplementation("io.micronaut:micronaut-http-client")
-    testImplementation("io.micronaut.test:micronaut-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
-    annotationProcessor("io.micronaut:micronaut-http-validation")
-    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+  mavenLocal()
+  mavenCentral()
 }
 
 application {
-    mainClass.set("com.nameplate.Application")
+  mainClass.set("com.nameplate.Application")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_24
-    targetCompatibility = JavaVersion.VERSION_24
+  sourceCompatibility = JavaVersion.VERSION_24
+  targetCompatibility = JavaVersion.VERSION_24
 }
 
 
-// ********************************************************************************************************************************
-// Micronaut Gradle Plugin options : https://micronaut-projects.github.io/micronaut-gradle-plugin/latest/#_micronaut_library_plugin
+// *************************************************************************************************************************************
+// PYTHON LIBRARIES Import *************************************************************************************************************
 
+graalPy {
+
+  // resourceDirectory.set("GRAALPY-VFS/com/nameplate")
+  // resourceDirectory.set("org.graalvm.python.vfs")
+
+  packages.set(
+          setOf(
+                  "--prefer-binary",
+                  wheelOsStandard,
+                  "numpy>=1.26.4",
+                  "python-dotenv>=1.1.1",
+                  "tqdm>=4.67.1",
+                  "PyYAML>=6.0.2",
+                  "pydantic>=2.11.7",
+                  "pillow",
+                  tikTokenInstall,
+                  shapelyInstall,
+                  sciKitLearnInstall,
+                  pandasInstall,
+                  sciPyInstall,
+                  paddlePaddleInstall,
+                  paddleOcrInstall,
+               )
+              )
+}
+
+// END PYTHON LIBRARIES Import *********************************************************************************************************
+// *************************************************************************************************************************************
+
+
+dependencies {
+  compileOnly("io.micronaut:micronaut-http-client")
+
+  implementation("org.graalvm.polyglot:polyglot:$graalPythonVersion")
+  implementation("org.graalvm.polyglot:python:$graalPythonVersion")
+
+  implementation("io.micronaut.views:micronaut-views-thymeleaf")
+  implementation("io.micronaut:micronaut-http-server-netty")
+  implementation("io.micronaut.graal-languages:micronaut-graalpy")
+  implementation("io.micronaut.serde:micronaut-serde-jackson")
+
+  runtimeOnly("org.yaml:snakeyaml")
+  runtimeOnly("ch.qos.logback:logback-classic")
+
+  testImplementation("io.micronaut:micronaut-http-client")
+  testImplementation("io.micronaut.test:micronaut-test-junit5")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+  annotationProcessor("io.micronaut:micronaut-http-validation")
+  annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+}
+
+
+// *************************************************************************************************************************************
+// Micronaut Gradle Plugin options : https://micronaut-projects.github.io/micronaut-gradle-plugin/latest/#_micronaut_library_plugin ****
 
 micronaut {
-    runtime("netty")
-    testRuntime("junit5")
-    processing {
-        incremental(true)
-        annotations("com.nameplate.*")
-    }
-    aot {
-        configFile = file("gradle/micronaut-aot.properties")
-    }
+  runtime("netty")
+  testRuntime("junit5")
+  processing {
+    incremental(true)
+    annotations("com.nameplate.*")
+  }
+  aot {
+    configFile = file("gradle/micronaut-aot.properties")
+  }
 }
 
-// END Micronaut ***********************************************************************************************************
-// *************************************************************************************************************************
+// END Micronaut ***********************************************************************************************************************
+// *************************************************************************************************************************************
 
 
-// *************************************************************************************************************************
-// GraalVM Gradle Plugin options : https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html ******************
+// *************************************************************************************************************************************
+// GraalVM Gradle Plugin options : https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html ******************************
 
 graalvmNative {
-    toolchainDetection = false
-    binaries {
-        named("main") {
-            imageName.set("nativeNameplateDataLogger")
-            richOutput.set(true)
-            verbose.set(true)
-            fallback.set(false)
-            mainClass.set("com.nameplate.Application")
-            resources.autodetect()
-            javaLauncher.set(javaToolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(24))
-                vendor.set(JvmVendorSpec.ORACLE)
-            })
-        }
-
-        named("optimized") {
-            imageName.set("optimizedNativeNameplateDataLogger")
-            richOutput.set(true)
-            verbose.set(true)
-            resources.autodetect()
-            mainClass.set("com.nameplate.Application")
-            resources.autodetect()
-            javaLauncher.set(javaToolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(24))
-                vendor.set(JvmVendorSpec.ORACLE)
-            })
-        }
+  toolchainDetection = false
+  binaries {
+    named("main") {
+      imageName.set("nativeNameplateDataLogger")
+      richOutput.set(true)
+      verbose.set(true)
+      fallback.set(false)
+      mainClass.set("com.nameplate.Application")
+      resources.autodetect()
+      javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(24))
+        vendor.set(JvmVendorSpec.ORACLE)
+      })
     }
+
+    named("optimized") {
+      imageName.set("optimizedNativeNameplateDataLogger")
+      richOutput.set(true)
+      verbose.set(true)
+      resources.autodetect()
+      mainClass.set("com.nameplate.Application")
+      resources.autodetect()
+      javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(24))
+        vendor.set(JvmVendorSpec.ORACLE)
+      })
+    }
+  }
 }
 
-// END GraalVM options ****************************************************************************************************
-// *************************************************************************************************************************
+// END GraalVM options *****************************************************************************************************************
+// *************************************************************************************************************************************
 
 
-//*************************************************************************************************************************
-// Dockerfile.graalpy-vfs instructions ************************************************************************************************
+// *************************************************************************************************************************************
+// Dockerfile.graalpy-vfs instructions *************************************************************************************************
 
 tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("optimizedDockerfileNative") {
-    jdkVersion.set("24")
-    graalImage.set("container-registry.oracle.com/graalvm/native-image:24.0.1")
-    baseImage.set("container-registry.oracle.com/graalvm/native-image:24.0.1")
-    exposedPorts.set(setOf(8181))
+  jdkVersion.set("24")
+  graalImage.set("container-registry.oracle.com/graalvm/native-image:24.0.1")
+  baseImage.set("container-registry.oracle.com/graalvm/native-image:24.0.1")
+  exposedPorts.set(setOf(8181))
 }
 
 // END Dockerfile.graalpy-vfs **********************************************************************************************************
-// *************************************************************************************************************************
+// *************************************************************************************************************************************
+
+
+tasks.withType<Jar> {
+  isZip64 = true
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+  isZip64 = true
+}
 
 
 //*************************************************************************************************************************
 // Python Resource Folder Management **************************************************************************************
 
+
 tasks.register<Copy>("copyVenvResources") {
-    group = "python"
-    description = "Copies GraalPy venv resources from build directory to .venv"
-    dependsOn("graalPyResources")
+  group = "python"
+  description = "Copies GraalPy venv resources from build directory to .venv"
+  dependsOn("graalPyResources")
 
-    from("build/generated/graalpy/resources/org.graalvm.python.vfs/venv") {
-        include("**/*")
-    }
-    into(layout.projectDirectory.dir(".venv"))
+  from("build/generated/graalpy/resources/org.graalvm.python.vfs/venv") {
+    include("**/*")
+  }
+  into(layout.projectDirectory.dir(".venv"))
 
-    doLast {
-        println("Copied GraalPy venv resources to .venv directory")
-    }
+  doLast {
+    println("Copied GraalPy venv resources to .venv directory")
+  }
 }
 
 tasks.named("graalPyResources") {
-    finalizedBy("copyVenvResources")
+  finalizedBy("copyVenvResources")
 }
 
 
-// This explicitly tells Gradle that processResources and processTestResources tasks depend on the graalPyResources task, ensuring proper task ordering.
-
-// tasks.named("processResources") {
-//     dependsOn("graalPyResources")
-// }
-// tasks.named("processTestResources") {
-//     dependsOn("graalPyResources")
-// }
-//
-// tasks.named("test") {
-//     dependsOn("graalPyResources")
-// }
-
-
-// This tells Gradle to include duplicate resources rather than failing the build when it encounters them.
-
-// tasks.withType<ProcessResources> {
-//     duplicatesStrategy = DuplicatesStrategy.INCLUDE
-// }
-//
-// sourceSets {
-//     main {
-//         resources {
-//             // Make sure main resources include the custom GraalPy VFS
-//             srcDir("${layout.buildDirectory}/generated/graalpy/resources/GRAALPY-VFS/com/nameplate/nameplate-data-logger")
-//         }
-//     }
-//     test {
-//         resources {
-//             // And your tests include it too
-//             srcDir("${layout.buildDirectory}/generated/graalpy/resources/GRAALPY-VFS/com/nameplate/nameplate-data-logger")
-//         }
-//     }
-// }
-
-
-// END Python Resource Folder Management **********************************************************************************
-//*************************************************************************************************************************
+// END Python Resource Folder Management ***********************************************************************************************
+// *************************************************************************************************************************************
--- buildSrc/src/main/kotlin/PipInstall.kt
+++ buildSrc/src/main/kotlin/PipInstall.kt
@@ -1,152 +1,61 @@
+import org.gradle.nativeplatform.OperatingSystemFamily.*
 import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import java.io.File
-import java.io.FilenameFilter
 
-private const val MACOS_WHL_DIR = "python-resources/MacOS/"
+private val OS_FAMILY_MAP: Map<String, String> = mapOf(
+        MACOS to "python-resources/MacOS/",
+        LINUX to "python-resources/Linux/",
+        WINDOWS to "python-resources/Windows/"
+                                                      )
 
 object PipInstall {
-    private val currentOS = DefaultNativePlatform.getCurrentOperatingSystem()
+  private val currentOS = DefaultNativePlatform.getCurrentOperatingSystem()
+  private val OS_SPECIFIC_PACKAGE_DIR = OS_FAMILY_MAP.getValue(currentOS.toFamilyName())
+  private const val ANY_OS_PACKAGE_DIR = "python-resources/any/"
 
-    enum class PackageName(val fallback: String) {
-        NUMPY("numpy==2.3.1"),
-        PILLOW("pillow==11.3.0"),
-        SHAPELY("shapely==2.1.1"),
-        PADDLEOCR("paddleocr==3.1.0"),
-        PADDLEPADDLE("paddlepaddle==3.1.0"),
-        PADDLEX("paddlex==3.1.1"),
-        SCIPY("scipy==1.16.1"),
-        PANDAS("pandas==2.3.0"),
-        SKITLEARN("scikit-learn==1.1.0"),
-        TIKTOKEN("tiktoken==0.9.0"),
+  enum class PackageName(val fallback: String) {
+    NUMPY("numpy>=2.3.1"),
+    PILLOW("pillow>=11.3.0"),
+    SHAPELY("shapely>=2.1.1"),
+    PADDLEOCR("paddleocr>=3.1.0"),
+    PADDLEPADDLE("paddlepaddle>=3.1.0"),
+    PADDLEX("paddlex>=3.1.1"),
+    SCIPY("scipy>=1.16.1"),
+    PANDAS("pandas>=2.3.0"),
+    SCIKIT_LEARN("scikit-learn>=1.1.0"),
+    TIKTOKEN("tiktoken>=0.9.0");
+
+    fun getPackage(rootDir: File): String {
+      return createFileInstall(PackageConfig(getFile(rootDir, OS_SPECIFIC_PACKAGE_DIR, name.lowercase()), fallback))
     }
+  }
 
-    val wheelOsStandard: String = when {
-        currentOS.isLinux -> "patchelf"
-        currentOS.isMacOsX -> "delocate"
-        currentOS.isWindows -> "delvewheel"
-        else -> "patchelf"
-    }
+  val wheelOsStandard: String = when {
+    currentOS.isLinux   -> "patchelf"
+    currentOS.isMacOsX  -> "delocate"
+    currentOS.isWindows -> "delvewheel"
+    else                -> "patchelf"
+  }
 
-    private data class PackageConfig(val path: String, val fallback: String)
+  data class PackageConfig(val path: String, val fallback: String)
 
-    private fun numpyConfig(rootDir: File): PackageConfig = when {
-        currentOS.isLinux -> PackageConfig(
-                File(
-                        rootDir,
-                        "python-resources/Linux/numpy-2.2.6-graalpy311-graalpy242_311_native-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl"
-                    ).absolutePath, PackageName.NUMPY.fallback
-                                          )
+  private fun findMatchingFile(directory: File, prefix: String): File? {
+    return directory.listFiles()
+            ?.firstOrNull { it.name.startsWith(prefix) }
+  }
 
-        currentOS.isMacOsX -> PackageConfig(
-                File(
-                        rootDir, "python-resources/MacOS/numpy-2.3.1-graalpy311-graalpy242_311_native-macosx_14_0_arm64.whl"
-                    ).absolutePath, PackageName.NUMPY.fallback
-                                           )
+  private fun getFile(rootDir: File, pathFromRoot: String, pkgNameContains: String): String {
+    val dir = File(rootDir, pathFromRoot)
+    val anyOsDir = File(rootDir, ANY_OS_PACKAGE_DIR)
 
-        else -> PackageConfig("", PackageName.NUMPY.fallback)
-    }
+    val pkgFile = findMatchingFile(dir, pkgNameContains) ?: findMatchingFile(anyOsDir, pkgNameContains)
 
-    private fun pillowConfig(rootDir: File): PackageConfig = when {
-        currentOS.isLinux -> PackageConfig(
-                File(
-                        rootDir,
-                        "python-resources/Linux/pillow-11.1.0-graalpy311-graalpy242_311_native-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl"
-                    ).absolutePath, PackageName.PILLOW.fallback
-                                          )
+    return pkgFile?.absolutePath ?: ""
+  }
 
-        currentOS.isMacOsX -> PackageConfig(
-                File(rootDir, "python-resources/MacOS/pillow-11.3.0-graalpy311-graalpy242_311_native-macosx_11_0_arm64.whl").absolutePath,
-                PackageName.PILLOW.fallback
-                                           )
+  private fun createFileInstall(packageConfig: PackageConfig): String {
+    return packageConfig.path.takeIf { it.isNotBlank() && File(it).exists() }
+                   ?.let { "file://$it" } ?: packageConfig.fallback
+  }
 
-        else -> PackageConfig("", PackageName.PILLOW.fallback)
-    }
-
-    private fun shapelyConfig(rootDir: File): PackageConfig = when {
-        currentOS.isLinux -> PackageConfig(
-                File(
-                        rootDir,
-                        "python-resources/Linux/shapely-2.1.1-graalpy311-graalpy242_311_native-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl"
-                    ).absolutePath, PackageName.SHAPELY.fallback
-                                          )
-
-        currentOS.isMacOsX -> PackageConfig(
-                File(rootDir, "python-resources/MacOS/shapely-2.1.1-graalpy311-graalpy242_311_native-macosx_14_0_arm64.whl").absolutePath,
-                PackageName.SHAPELY.fallback
-                                           )
-
-        else -> PackageConfig("", PackageName.SHAPELY.fallback)
-    }
-
-    private fun paddlePaddleConfig(rootDir: File): PackageConfig = when {
-        currentOS.isLinux -> PackageConfig(
-                File(
-                        rootDir, "python-resources/Linux/paddlepaddle-3.0.0-graalpy311-graalpy242_311_native-manylinux2014_aarch64.whl"
-                    ).absolutePath, PackageName.PADDLEPADDLE.fallback
-                                          )
-
-        currentOS.isMacOsX -> PackageConfig(
-                File(rootDir, "python-resources/MacOS/paddlepaddle-3.1.0-graalpy311-graalpy242_311_native-macosx_11_0_arm64.whl").absolutePath,
-                PackageName.PADDLEPADDLE.fallback
-                                           )
-
-        else -> PackageConfig("", PackageName.PADDLEPADDLE.fallback)
-    }
-
-    private fun paddleOcrConfig(rootDir: File): PackageConfig = PackageConfig(
-            File(rootDir, "python-resources/any/paddleocr-3.1.0-py3-none-any.whl").path,
-            PackageName.PADDLEOCR.fallback
-                                                                             )
-
-    private fun paddleXConfig(rootDir: File): PackageConfig = PackageConfig(
-            File(rootDir, "python-resources/any/paddlex-3.1.1-py3-none-any.whl").path,
-            PackageName.PADDLEOCR.fallback
-                                                                           )
-
-    private fun sciPyConfig(rootDir: File): PackageConfig = PackageConfig(
-            File(rootDir, "python-resources/MacOS/scipy-1.16.0-graalpy311-graalpy242_311_native-macosx_14_0_arm64.whl").path,
-            PackageName.SCIPY.fallback
-                                                                         )
-
-    private fun pandasConfig(rootDir: File): PackageConfig = PackageConfig(
-            File(rootDir, "python-resources/MacOS/pandas-2.3.0-graalpy311-graalpy242_311_native-macosx_11_0_arm64.whl").path,
-            PackageName.PANDAS.fallback
-                                                                          )
-
-    private fun skitLearnConfig(rootDir: File): PackageConfig = PackageConfig(
-            File(rootDir, "python-resources/MacOS/scikit_learn-1.7.0-graalpy311-graalpy242_311_native-macosx_12_0_arm64.whl").path,
-            PackageName.SKITLEARN.fallback
-                                                                             )
-
-    private fun tikTokenConfig(rootDir: File): PackageConfig {
-        val dir = File(rootDir, MACOS_WHL_DIR)
-        val tikTokenFile = dir.listFiles(FilenameFilter { _, name -> name.startsWith("tiktoken") })
-            ?.firstOrNull()
-        val path = tikTokenFile?.absolutePath ?: ""
-        return PackageConfig(path, PackageName.TIKTOKEN.fallback)
-    }
-
-    private fun createFileInstall(packageConfig: PackageConfig): String {
-        val validPath = packageConfig.path.takeIf { it: String ->
-            it.isNotBlank() && File(it).exists()
-        }
-            ?.let { "file://$it" }
-
-        return validPath ?: packageConfig.fallback
-    }
-
-    fun getPackageBinary(rootDir: File, packageName: PackageName): String {
-        return when (packageName) {
-            PackageName.NUMPY -> createFileInstall(numpyConfig(rootDir))
-            PackageName.PILLOW -> createFileInstall(pillowConfig(rootDir))
-            PackageName.SHAPELY -> createFileInstall(shapelyConfig(rootDir))
-            PackageName.PADDLEOCR -> createFileInstall(paddleOcrConfig(rootDir))
-            PackageName.PADDLEPADDLE -> createFileInstall(paddlePaddleConfig(rootDir))
-            PackageName.PADDLEX -> createFileInstall(paddleXConfig(rootDir))
-            PackageName.SCIPY -> createFileInstall(sciPyConfig(rootDir))
-            PackageName.PANDAS -> createFileInstall(pandasConfig(rootDir))
-            PackageName.SKITLEARN -> createFileInstall(skitLearnConfig(rootDir))
-            PackageName.TIKTOKEN -> createFileInstall(tikTokenConfig(rootDir))
-        }
-    }
 }
